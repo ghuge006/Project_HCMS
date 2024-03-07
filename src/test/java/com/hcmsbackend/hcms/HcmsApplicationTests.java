ee8cc3cab0944be47782dfbcfdbb83e9bede6ea8
unknown <akshay.ghuge006@gmail.com>
2024-03-08 00:16:10 +0530

    complete all DAO & services

---
+++ src/test/java/com/hcmsbackend/hcms/HcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmsbackend.hcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
